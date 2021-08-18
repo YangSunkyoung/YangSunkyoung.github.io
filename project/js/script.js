775365146b1d7be4dd1eba0d39fbaf457856b90b
YangSunkyoung <88478817+YangSunkyoung@users.noreply.github.com>
2021-08-19 03:23:07 +0900

    Add files via upload

--- project/js/script.js
+++ project/js/script.js
@@ -1,20 +1,16 @@
+<script type="text/javascript">
+const openButton = document.getElementById("open");
+const modal = document.querySelector(".modal");
+const overlay = modal.querySelector(".md_overlay");
+const closeButton = modal.querySelector("button");
 
-		const openButton = document.getElementById("open");
-		const modal = document.querySelector(".modal");
-		const overlay = modal.querySelector(".md_overlay");
-		const closeButton = modal.querySelector("button");
-		//동작함수
-		const openModal = () => {
-			modal.classList.remove("hidden");
-		}
-		const closeModal = () => {
-			modal.classList.add("hidden");
-		}
-		//클릭 이벤트
-		openButton.addEventListener("click", openModal);
-		closeButton.addEventListener("click", closeModal);
-	//-->
-	
+const openModal = ()  {
+  modal.classList.remove("hidden")
+}
+const closeModal = () {
+  modal.classList.add("hidden")
+}
+openButton.addEventListener("click", openModal);
+closeButton.addEventListener("click", closeModal);
 
-
-
+</script>
