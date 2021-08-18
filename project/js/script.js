92f3a0807f1c9e203bc8c2e952a0b5d4d53d83bf
YangSunkyoung <88478817+YangSunkyoung@users.noreply.github.com>
2021-08-18 16:37:15 +0900

    Add files via upload

---
+++ project/js/script.js
@@ -0,0 +1,20 @@
+
+		const openButton = document.getElementById("open");
+		const modal = document.querySelector(".modal");
+		const overlay = modal.querySelector(".md_overlay");
+		const closeButton = modal.querySelector("button");
+		//동작함수
+		const openModal = () => {
+			modal.classList.remove("hidden");
+		}
+		const closeModal = () => {
+			modal.classList.add("hidden");
+		}
+		//클릭 이벤트
+		openButton.addEventListener("click", openModal);
+		closeButton.addEventListener("click", closeModal);
+	//-->
+	
+
+
+
